be8917f116ae11293734a288ff779f66f4553337
Clara Colaço <colacoclara0@gmail.com>
2026-03-30 09:28:51 -0300

    Adiciona atividade 03 - Protocolo MQTT com projeto mqtt_ifpb

---
+++ 03 - Protocolo MQTT/mqtt_ifpb/main/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "mqtt_client.h"
+#include "esp_log.h"
+#include "esp_err.h"
+
+static const char *TAG = "MQTT_TUTORIAL";
+
+#define BROKER_URL "mqtt://192.168.0.3"
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "Conectado ao Broker!");
+            esp_mqtt_client_publish(event->client, "ifpb/c6/status", "ESP32 Ativo", 0, 1, 0);
+            break;
+
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGW(TAG, "MQTT Desconectado");
+            break;
+
+        default:
+            break;
+    }
+}
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_LOGI(TAG, "Conectando ao Wi-Fi...");
+    ESP_ERROR_CHECK(example_connect());
+    ESP_LOGI(TAG, "Wi-Fi conectado!");
+
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URL,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
